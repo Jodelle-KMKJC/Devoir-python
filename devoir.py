e894908ba60dd7b995f55c1b2722d1cd2f8b8c31
cabrellekamgaing@gmail.com <cabrellekamgaing@gmail.com>
2025-07-14 04:09:37 +0100

    python devoir

---
+++ devoir.py
@@ -0,0 +1,33 @@
+import math
+
+class couleur;
+    def __init__(self, nom, code_hex):
+        self.nom = nom
+        self.code_hex = code_hex
+
+    def afficher_info(self);
+        return *couleur : {} (code : {})*.format(self.nom, self.code_hex):
+
+if __name__ == "__main__":
+        rouge = _("rouge" , "red")
+        bleu = _("bleu" , "#0000FF")
+        vert= _("vert" , "#00FF00")
+        jaune= _("jaune" , "#FFFF00")
+
+    class figure;
+        def __init__(self, nom, couleur):
+            self.couleur = couleur
+            self.nom = nom
+
+        def afficher_info(self):
+            return "figure : {} -{}".format(self.nom, self.couleur.afficher_info())
+
+        def aire(self):
+            return 0
+
+        def perimetre(self):
+            return 0
+
+        def afficher_resultats(self)
+            print("+_)(*&&&7&!2>>ZX)")
+
